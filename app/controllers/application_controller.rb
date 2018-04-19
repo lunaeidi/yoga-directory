c230f5675a00b038c626395c6161e7840f7384d0
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-19 16:25:07 -0700

    set up file structure

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :logged_in?
+  def logged_in?
+    !!session[:user_id]
+  end
+  def current_user
+       User.find(session[:user_id])
+     end
+end
