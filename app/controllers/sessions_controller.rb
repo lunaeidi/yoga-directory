31865db9e2651ec1ab7921c96b7a37a49ed284ab
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-19 17:37:58 -0700

    created users table and added omniauth logic to controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,17 @@
 class SessionsController < ApplicationController
   def new
-    if
+
   end
-  def create
+  def create #need email too because of omniauth option
+  if auth = request.env['omniauth.auth']
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+    end
+  session[:user_id] = @user.id
+render 'users/home' #is this interchangeable with redirect_to controller:'....' or can do redirect_to root_path
+
+  else #login normal way
      user = User.find_by(:username => params[:user][:username])
      if user && user.authenticate(params[:user][:password])
        session[:user_id]= user.id
@@ -11,6 +20,7 @@ class SessionsController < ApplicationController
        flash[:message]= "Invalid username or password" #does this work? u need the middleware use flash? or just :alert?
        redirect_to controller: 'sessions', action: 'new'
      end
+   end
   end
   def destroy
     session.clear
