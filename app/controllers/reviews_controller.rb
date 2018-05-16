5e5e990570b884ed06fb2a63583a154e0b74ef3f
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-16 14:48:08 -0700

    add review create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,8 @@
 class ReviewsController < ApplicationController
+  def create
+    Review.create(content: params[:review][:content],user_id: params[:review][:user_id], pose_id:params[:review][:pose_id])
+
+    @pose = Pose.find(params[:review][:pose_id])
+    redirect_to pose_path(@pose)
+  end
 end
