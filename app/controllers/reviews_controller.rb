2fc60452a51ccac40c06c30327c5a5cc42706640
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-20 19:19:51 -0700

    start post review AJAX functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,14 @@
 class ReviewsController < ApplicationController
-  def create
-    Review.create(content: params[:review][:content],user_id: params[:review][:user_id], pose_id:params[:review][:pose_id])
+  # def create
+  #   Review.create(content: params[:review][:content],user_id: params[:review][:user_id], pose_id:params[:review][:pose_id])
+  #
+  #   @pose = Pose.find(params[:review][:pose_id])
+  #   redirect_to pose_path(@pose)
+  # end
 
-    @pose = Pose.find(params[:review][:pose_id])
-    redirect_to pose_path(@pose)
-  end
+  def create
+  @review = Review.create(content: params[:review][:content],user_id: params[:review][:user_id], pose_id:params[:review][:pose_id])
+  render json: @review, status: 201
+end
+
 end
