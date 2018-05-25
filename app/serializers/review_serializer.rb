157b207f0f51f5b8971a56c4f6d7685930951d2f
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-24 17:31:06 -0700

    fix next feature and add username to post-review feature

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,8 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :pose_id, :content #, :username
+  attributes :id, :user_id, :pose_id, :content , :username
+  def username
+  #  binding.pry
+    u= User.find(object.user_id)
+    u.username
+  end
 end
