99f1bd077003020ec3deaca9f82bb4d32db019ef
Nicole Eidi <nicoleeidi1@gmail.com>
2018-08-11 12:55:10 -0700

    fix to post review bug

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,72 +1,67 @@
-
-
-  $(function () {
-    $(".loadReviews").on("click", function() {
-      let poseid = $(this).attr("data-id");
-        $.get(`/poses/${poseid}.json`, function(data) {
-        let reviews= data["reviews"]
-
-        // sort by name
-      let sorted=  reviews.sort(function(a, b) {
-          var contentA = a["content"].toUpperCase(); // ignore upper and lowercase
-          var contentB = b["content"].toUpperCase(); // ignore upper and lowercase
-          if (contentA < contentB) {
-            return -1;
-          }
-          if (contentA > contentB) {
-            return 1;
-          }
-
-          // names must be equal
-          return 0;
-        });
-
-      // let sorted=  reviews.sort(function(a,b){
-      //     return a["content"] - b["content"]
-      //   })
-
-
-
-
-        //reviews.forEach(function(review){
-        sorted.forEach((review)=>{
-
-          $('#load-review').append("<li>"+review["username"] + " says " + review["content"]+"</li>")
-
-        })
-      })
-    })
-  })
-
-
-
-  $(function () {
-    $('.reviewform').submit(function(event) {
-
-      event.preventDefault();
-      event.stopPropagation()
-        function Review(username, content) {
-          this.username = username;
-          this.content = content;
-        }
-        Review.prototype.postReview = function() {
-            return `${this.username} says ${this.content} `
-        }
-
-  let id = parseInt($(".js-next").attr("data-id"))
-
-      var posting = $.post('/reviews', $(this).serialize());
-
-      posting.done(function(data) {
-        $("#review_content").val("")
-       let post = data;
-
-
-
-       let review2 = new Review(post["username"], post["content"]);
-          $("#review").html("<li>"+ review2.postReview() + "</li>")
-
-
-      });
-    });
-  });
+//
+//
+//   $(function () {
+//     $(".loadReviews").on("click", function() {
+//       console.log("A")
+//       let poseid = $(this).attr("data-id");
+//         $.get(`/poses/${poseid}.json`, function(data) {
+//         let reviews= data["reviews"]
+//
+//         // sort by name
+//       let sorted=  reviews.sort(function(a, b) {
+//           var contentA = a["content"].toUpperCase(); // ignore upper and lowercase
+//           var contentB = b["content"].toUpperCase(); // ignore upper and lowercase
+//           if (contentA < contentB) {
+//             return -1;
+//           }
+//           if (contentA > contentB) {
+//             return 1;
+//           }
+//
+//           return 0;
+//         });
+//
+//         //reviews.forEach(function(review){
+//         sorted.forEach((review)=>{
+//           console.log("B")
+//
+//           $('#load-review').append("<li>"+review["username"] + " says " + review["content"]+"</li>")
+//
+//         })
+//       })
+//     })
+//   })
+//
+//
+//
+//   $(function () {
+//     $('.reviewform').submit(function(event) {
+//
+// console.log("D")
+//       event.preventDefault();
+//       event.stopPropagation()
+//         function Review(username, content) {
+//           this.username = username;
+//           this.content = content;
+//         }
+//         Review.prototype.postReview = function() {
+//             return `${this.username} says ${this.content} `
+//         }
+//
+//   let id = parseInt($(".js-next").attr("data-id"))
+//
+//       let posting = $.post('/reviews', $(this).serialize());
+// console.log("C")
+//       posting.done(function(data) {
+//         $("#review_content").val("")
+//        let post = data;
+//
+//
+//
+//        let review2 = new Review(post["username"], post["content"]);
+//           $("#review").html("<li>"+ review2.postReview() + "</li>")
+//
+//
+//       });
+//     });
+//   });
